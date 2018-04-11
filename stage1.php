847c895d7f10daf82b06db63dff43dc534c479b8
offiahchiaz <offiahchiaz@gmail.com>
2018-04-11 19:14:15 +0100

    Initial commmit

---
+++ stage1.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>TechBlog :: Profile</title>
+		<link href="css/stage1.css" rel="stylesheet" type="text/css" media="all" />
+	</head>
+	<body>
+
+		<div class="content">
+			<span id="kyle">Kyle Simpson</span>&nbsp; | &nbsp; 
+		<small>Software Developer, Co-founder, Teacher</small>
+		<span id="logo">
+			<img src="images/23931.svg" alt="" style="width: 20px; height: 13.09px;">
+			<img src="images/06-facebook-512.png" alt="" style="width: 15px; height: 15px;">
+			<img src="images/phone.png" alt="" style="width: 13px; height: 13px;">
+		</span>
+		 
+		<div class="writeup">
+			<p id="time">
+			<?
+			date_default_timezone_set("Africa/Lagos");
+			echo "Local time: " .  date(" h:i a");
+			?>
+				
+			</p>
+				<img id="profileImage" src="images/150330.jpg" alt="">
+				<p class="p1">
+					Kyle Simpson is an Open Web Evangelist who's passionate about all things JavaScript.
+				</p>
+				<p class="p1">
+					He's an author, workshop trainer, tech speaker, and OSS contributor/leader.
+				</p>
+				<p class="p1">
+					Kyle is a JavaScript enthusiast, a teacher, a mentor, a lifelong advocate of the OpenSource philosophy who is now working on DevGo
+				</p>
+		</div>
+		<footer>
+			<span id="span1">
+				2018 &copy; TechBlog
+			</span>
+			<span id="span2">Powered by rapidGraphix</span>
+		</footer>
+		</div>
+	</body>
+</html>
+
